ec44a302ba157ac000087452c159fe044e693b92
Rachel Gopman <159563043+lapizwaves@users.noreply.github.com>
2025-04-17 03:31:51 -0400

    Add files via upload
    updated linked and /body and /html

--- ContactUs.php
+++ ContactUs.php
@@ -16,18 +16,18 @@
       <img src="https://cdn.builder.io/api/v1/image/assets/3a8ac60b581045f7adb5757904dc023c/9e595b5f1fcde182a7dfc4c426d1f05c6b2789f1?placeholderIfAbsent=true" alt="Logo" class="main-logo" />
       <nav class="navigation-bar">
         <div class="nav-links">
-          <a href="HomePage.html" class="nav-link">Home</a>
-          <a href="HowItWorks.html" class="nav-link">How It Works</a>
-          <a href="Countries.html" class="nav-link">Countries</a>
-          <a href="GetStartedQ1.html" class="nav-link">Get Started</a>
+          <a href="HomePage.php" class="nav-link">Home</a>
+          <a href="HowItWorks.php" class="nav-link">How It Works</a>
+          <a href="Countries.php" class="nav-link">Countries</a>
+          <a href="GetStartedQ1.php" class="nav-link">Get Started</a>
         </div>
         <div class="nav-icons">
 
-            <a href="ShoppingCart.html">
+            <a href="ShoppingCart.php">
           <img src="https://cdn.builder.io/api/v1/image/assets/3a8ac60b581045f7adb5757904dc023c/edadf950090628a4467326f5ac2e7e6a6c82bdb8?placeholderIfAbsent=true" alt="User Icon" class="nav-icon" />
         </a>
           
-          <a href="Profile.html">
+          <a href="Profile.php">
           <img src="https://cdn.builder.io/api/v1/image/assets/3a8ac60b581045f7adb5757904dc023c/81178d926783336ee4924fea04237c405ade17aa?placeholderIfAbsent=true" alt="Menu Icon" class="nav-icon menu-icon" />
         </a>
         
@@ -115,15 +115,19 @@
       </div>
   
       <nav class="footer-nav">
-        <a href="FAQs.html" class="footer-link">FAQ</a>
-        <a href="ContactUs.html" class="footer-link">Contact Us</a>
-        <a href="HowItWorks.html" class="footer-link">How it Works</a>
-        <a href="Countries.html" class="footer-link">Countries</a>
-        <a href="GetStartedQ1.html" class="footer-link">Get Started</a>
+        <a href="FAQs.php" class="footer-link">FAQ</a>
+        <a href="ContactUs.php" class="footer-link">Contact Us</a>
+        <a href="HowItWorks.php" class="footer-link">How it Works</a>
+        <a href="Countries.php" class="footer-link">Countries</a>
+        <a href="GetStartedQ1.p" class="footer-link">Get Started</a>
       </nav>
   
       <img src="https://cdn.builder.io/api/v1/image/assets/3a8ac60b581045f7adb5757904dc023c/83a0146325637aabd8615d230e39b2f36738afd0?placeholderIfAbsent=true" alt="Footer Logo 1" class="footer-logo" />
       <img src="https://cdn.builder.io/api/v1/image/assets/3a8ac60b581045f7adb5757904dc023c/9c2fd45f0841e4cf7d570ccfb6099e596290f28e?placeholderIfAbsent=true" alt="Footer Logo 2" class="footer-logo secondary" />
     </div>
   </footer>
+
+  
+</body>
+</html>
   
